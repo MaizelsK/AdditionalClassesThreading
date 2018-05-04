620e04fcb4a10b323df15efe623c39f5da516ffa
Майзельс Кирилл Игоревич <@corp.itstep>
2018-05-04 10:38:53 +0600

    First commit

---
+++ AdditionalClassesThreading/Printer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Contexts;
+
+namespace AdditionalClassesThreading
+{
+    [Synchronization]
+    public class Printer : ContextBoundObject
+    {
+        private object lockObject = new object();
+        public void Print()
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine("Работает поток " + Thread.CurrentThread.Name);
+                for (int i = 0; i < 10; i++)
+                {
+                    Random random = new Random();
+
+                    Thread.Sleep(5 * random.Next(150));
+                    Console.WriteLine(i);
+                }
+            }
+
+            #region Semaphore
+            //Semaphore semaphore = new Semaphore(50, 3);
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Random random = new Random();
+
+            //    Thread.Sleep(5 * random.Next(150));
+            //    Console.WriteLine(i);
+            //}
+            //semaphore.WaitOne();
+            #endregion
+
+            #region Monitor
+            // Тоже самое через Monitor
+
+            //Monitor.Enter(lockObject);
+            //try
+            //{
+            //    {
+            //        for (int i = 0; i < 10; i++)
+            //        {
+            //            Random random = new Random();
+            //
+            //            Thread.Sleep(5 * random.Next(150));
+            //            Console.Write(i);
+            //        }
+            //    }
+            //}
+            //finally
+            //{
+            //    Monitor.Exit(lockObject);
+            //}
+            #endregion
+        }
+    }
+}
---
+++ AdditionalClassesThreading/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AdditionalClassesThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+
+            Thread[] threads = new Thread[50];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(printer.Print));
+                threads[i].Name = "Поток №" + i;
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+
+            #region Thread
+            for (int i = 0; i < 50; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(
+                    state =>
+                    {
+                        ((Printer)state).Print();
+                    }), printer);
+            }
+            #endregion
+
+            // Таймер на выполнение какого-либо метода
+            Timer timer = new Timer(new TimerCallback(stubObject => Console.WriteLine("Выполняется работа")),
+                                    null, 500, 2000);
+
+            Console.ReadLine();
+        }
+    }
+}
